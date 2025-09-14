391e593de3ccb6951e1533a5dd1b463bebc53634
xArkya <fabricio.raimondo2@gmail.com>
2025-09-14 05:02:18 -0300

    w

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Para GitHub Pages project site (xarkya.github.io/arkya.store)
-  base: '/arkya.store/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
