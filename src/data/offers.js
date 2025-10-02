a56f3b06aea77bd24b1d1d085de9389ea73f295b
xArkya <fabricio.raimondo2@gmail.com>
2025-10-02 16:53:10 -0300

    sorteo fin

--- src/data/offers.js
+++ src/data/offers.js
@@ -5,13 +5,15 @@ export const offers = [
     "description": "10% de descuento en todos los productos de la tienda",
     "discountPercentage": 10,
     "discountType": "percentage",
-    "isActive": true,
+    "isActive": false,
     "isGlobal": true,
     "offerType": "descuento",
     "applicableCategories": [],
     "applicableProducts": [],
     "startDate": "",
     "endDate": "",
-    "minPurchaseAmount": 0
+    "minPurchaseAmount": 0,
+    "createdAt": "2025-10-02T19:47:30.262Z",
+    "updatedAt": "2025-10-02T19:47:30.262Z"
   }
 ];
